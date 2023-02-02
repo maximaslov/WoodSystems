b756c5f521bf2dd3ef1e43deea8bf1c4b67752e7
Maxim <masmaxvad@gmil.com>
2023-02-02 10:57:55 +0200

    test swype and test menu item

--- script.js
+++ script.js
@@ -3,7 +3,9 @@
 const mobileMenuBtn = document.querySelector('.header__menu-mobile-btn');
 const mobileMenu = document.querySelector('.header__menu-mobile');
 const mobileMenuLine = document.querySelector('.header__menu-mobile-separator');
+const mobileMenuItem = document.querySelector('.header__menu-mobile-item');
 
+console.log(mobileMenuItem)
 mobileMenuBtn.addEventListener('click', onMobileMenuBtnClick);
 mobileMenuLine.addEventListener('click', onMobileMenuLineClick);
 
@@ -18,3 +20,11 @@ function onMobileMenuBtnClick() {
 function onMobileMenuLineClick () {
     onMobileMenuBtnClick();
 }
+
+mobileMenuItem.addEventListener('click' , () => alert('its work'))
+
+mobileMenu.addEventListener('touchend', function(e){
+    if (e.changedTouches[0].clientY < e.target.clientHeight) {
+        onMobileMenuBtnClick();
+    }
+    });
