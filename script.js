3da850019a2816d35389c89adb6d447c08ba5d6b
Maxim <masmaxvad@gmil.com>
2023-02-02 11:12:45 +0200

    body overflow

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 'use strict'
 
+const mainContainer = document.querySelector('.container');
+const body = document.getElementsByTagName('body')[0];
 const mobileMenuBtn = document.querySelector('.header__menu-mobile-btn');
 const mobileMenu = document.querySelector('.header__menu-mobile');
 const mobileMenuLine = document.querySelector('.header__menu-mobile-separator');
 const mobileMenuItem = document.querySelector('.header__menu-mobile-item');
 
-console.log(mobileMenuItem)
 mobileMenuBtn.addEventListener('click', onMobileMenuBtnClick);
 mobileMenuLine.addEventListener('click', onMobileMenuLineClick);
 
@@ -14,6 +15,11 @@ function onMobileMenuBtnClick() {
     setTimeout(()=> {
         mobileMenu.classList.toggle('active');
         mobileMenuBtn.classList.toggle('active');
+        if(mobileMenu.classList.contains('active')) {
+            body.style.overflow = 'hidden';
+        } else {
+            body.style.overflow = 'auto';
+        }
     }, 1)
 }
 
