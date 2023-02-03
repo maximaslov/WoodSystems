875c23852253732fd0fd7f0d0b2de84574f6499a
Maxim <masmaxvad@gmil.com>
2023-02-03 18:16:44 +0200

    press page rsponsive + js done

--- script.js
+++ script.js
@@ -5,9 +5,15 @@ const innovationBtn = document.querySelector('.innovations-btn')
 const mobileMenuBtn = document.querySelector('.header__menu-mobile-btn');
 const mobileMenu = document.querySelector('.header__menu-mobile');
 const mobileMenuLine = document.querySelector('.header__menu-mobile-separator');
+const pressImage = document.querySelectorAll('.press__image-item');
+const pressImageContainer = document.querySelectorAll('.press__image');
+const pressContainer = document.querySelector('.press');
 
 mobileMenuBtn.addEventListener('click', onMobileMenuBtnClick);
 mobileMenuLine.addEventListener('click', onMobileMenuLineClick);
+mobileMenu.addEventListener('touchend', onMobileMenuSwypeUp);
+pressContainer.addEventListener('click', onPressContainerClick);
+// pressImageEventListener();
 
 function onMobileMenuBtnClick() {
     mobileMenu.style.display = 'block';
@@ -26,8 +32,51 @@ function onMobileMenuLineClick () {
     onMobileMenuBtnClick();
 }
 
-mobileMenu.addEventListener('touchend', function(e){
+function onMobileMenuSwypeUp (e) {
     if (e.changedTouches[0].clientY < e.target.clientHeight) {
         onMobileMenuBtnClick();
     }
-    });
+}
+// mobileMenu.addEventListener('touchend', function(e){
+//     if (e.changedTouches[0].clientY < e.target.clientHeight) {
+//         onMobileMenuBtnClick();
+//     }
+//     });
+
+// pressImage.addEventListener('click', onPressImageClick);
+
+// function onPressImageClick(e) {
+//     e.target.classList.toggle("active");
+// }
+
+function onPressContainerClick(e) {
+    if(e.target.classList.contains('press__image-block')
+        || e.target === pressContainer) {
+
+        pressImageContainer.forEach(elem => {
+            if(elem.classList.contains('active')){
+                elem.classList.remove('active');
+            } 
+        });
+
+        // pressImage.forEach((elem) => {
+        //     if(elem.classList.contains('active')){
+        //         elem.classList.remove('active');
+        //         setTimeout(() => { 
+        //             elem.classList.remove('index')
+        //         },300)
+        //     } 
+        // });
+    }
+    
+    if(e.target.classList.contains("press__image-item")) {
+        pressImageContainer.forEach(elem => {
+            if(elem.classList.contains('active')){
+                elem.classList.remove('active');
+            } 
+        });
+
+        const parent = e.target.parentElement;
+        parent.classList.toggle("active");
+    }
+}
